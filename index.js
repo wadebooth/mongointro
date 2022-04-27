09e6b74438d5ee218188d7e7946b1084ea4e3588
wadebooth <wademgmt@gmail.com>
2022-04-27 11:27:23 -0400

    created database with users

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import mongodb from "mongodb";
+import mongodb from "mongodb"; //Always do this
 
 const client = new mongodb.MongoClient(
   "mongodb+srv://wadebooth:Password@cluster0.nrzns.mongodb.net?retryWrites=true&w=majority"
@@ -6,3 +6,12 @@ const client = new mongodb.MongoClient(
 
 await client.connect();
 console.log("Connected!");
+const db = client.db("trial"); // Creates database
+const collection = db.collection("users");
+await collection.insertOne({
+  firstName: "Manny",
+  lastName: "Toribio",
+  carCrash: true,
+});
+
+await client.close();
